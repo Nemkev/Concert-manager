c1ce57b2d5349e9df4eb4edebc648f04673cd060
Zhenya Nemkevich <ostegoelectric@gmail.com>
2019-11-18 14:08:22 +0300

    feat: connect validation on building

--- src/services/buildingValidate.js
+++ src/services/buildingValidate.js
@@ -3,12 +3,20 @@ import joi from "joi";
 export default joi.object().keys({
   rooms: joi
     .array()
-    .required()
     .min(1)
     .max(5),
   concerts: joi
     .array()
     .max(7)
     .min(1),
-  additionalId: joi.array()
+  additionalId: joi.array(),
+  city: joi
+    .string()
+    .min(2)
+    .max(40),
+  name: joi
+    .string()
+    .min(2)
+    .max(20),
+  id: joi.string()
 });
