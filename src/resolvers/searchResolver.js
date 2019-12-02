7b630f00b9f3f2db5ca906a6557d406468801ef1
Zhenya Nemkevich <ostegoelectric@gmail.com>
2019-12-02 16:43:31 +0300

    feat: add search by input

--- src/resolvers/searchResolver.js
+++ src/resolvers/searchResolver.js
@@ -1,8 +1,8 @@
 import db from "../models/db";
 export default {
   Query: {
-    searchConcerts: async (_, { text }) => {
-      return await db.Concert.find({ name: /Hell/i });
+    searchConcerts: async (_, { name }) => {
+      return await db.Concert.find({ name: new RegExp(`${name}`) });
     }
   }
 };
