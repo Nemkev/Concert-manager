b7c7964d54c98b7f8bd4a9d92428842aa36d5dbb
Zhenya Nemkevich <ostegoelectric@gmail.com>
2019-12-05 16:34:29 +0300

    feat: add filter

--- src/resolvers/filterResolver.js
+++ src/resolvers/filterResolver.js
@@ -3,33 +3,24 @@ import db from "../models/db";
 export default {
   Query: {
     getFilter: async (_, { name, city, date, limit, skip }) => {
-      const data = await db.Building.find(
-        {
-          $and: [
-            {
-              name: new RegExp(`${name}`)
-            }
-          ],
-          $or: [
-            {
-              city: new RegExp(`${city}`)
-            }
-          ]
+      const data = await db.Building.find({}, null, { limit, skip })
+        .populate({ path: "concerts", model: db.Concert })
+        .lean();
 
-          //   $or: [
-          //     {
-          //       date: { $elemMatch: { $in: [new RegExp(`${date}`)] } }
-          //     }
-          //   ]
-        },
-        null,
-        { limit, skip }
-      ).populate({ path: "concerts", model: db.Concert });
+      return data
+        .filter(item => (city ? item.city.includes(city) : item))
+        .filter(item => (name ? item.name.includes(name) : item))
+        .map(item => {
+          if (date) {
+            const concerts = item.concerts
+              .map(item => Object.assign({}, item, { id: item._id }))
+              .filter(item => ("" + item.date).includes(date));
 
-      //   data.find(date === data.concerts);
-      console.log(data.find(() => date === data.concerts));
+            return Object.assign({}, item, { concerts, id: item._id });
+          }
 
-      return data;
+          return item;
+        });
     }
   }
 };
