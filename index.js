dac69dfc49054960ab6cfb6a584c397c46b36c4e
Zhenya Nemkevich <ostegoelectric@gmail.com>
2020-01-17 11:37:24 +0300

    feat: add sockets to refactor

--- index.js
+++ index.js
@@ -37,11 +37,23 @@ const startServer = async () => {
   const server = http.Server(app);
   const io = socketIO(server);
 
-  io.on("connection", client => {
+  io.on("connection", socket => {
     console.log("New client connected");
-    client.on("subscribeToTimer", data => {
-      console.log("New client connected to subscribeInterval");
-      io.emit("broadcast", data);
+
+    socket.on("username", function(username) {
+      socket.username = username;
+      io.emit("is_online", "🔵 <i>" + socket.username + " join the chat..</i>");
+    });
+
+    socket.on("disconnect", function(username) {
+      io.emit("is_online", "🔴 <i>" + socket.username + " left the chat..</i>");
+    });
+
+    socket.on("chat_message", function(message) {
+      io.emit(
+        "chat_message",
+        "<strong>" + socket.username + "</strong>: " + message
+      );
     });
   });
 
